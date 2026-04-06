9bae62e16adbb8a91767e560087426803bf16a8d
darren <darren.jensen@gmail.com>
2026-04-06 19:53:50 +0700

    added support for OpenAI or Ollama

--- main.py
+++ main.py
@@ -11,12 +11,19 @@ from prompts import link_system_prompt, brochure_system_prompt
 
 load_dotenv()
 
-OLLAMA_BASE_URL = os.getenv("OLLAMA_BASE_URL", "http://localhost:11434/v1")
-MODEL = os.getenv("MODEL", "llama3.1:8b")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-openai = OpenAI(base_url=OLLAMA_BASE_URL, api_key='ollama')
-print(f"Using model: {MODEL}")
-print(f"Using Ollama URL:: {OLLAMA_BASE_URL}")
+if OPENAI_API_KEY:
+    print("Using OpenAI")
+    MODEL = os.getenv("OPENAI_MODEL", "gpt-5-nano")
+    openai = OpenAI(api_key=OPENAI_API_KEY)
+    print(f"Using OpenAI with model: {MODEL}")
+else:
+    print("Using Ollama")
+    OLLAMA_BASE_URL = os.getenv("OLLAMA_BASE_URL", "http://localhost:11434/v1")
+    MODEL = os.getenv("OLLAMA_MODEL", "llama3.1:8b")
+    openai = OpenAI(base_url=OLLAMA_BASE_URL, api_key='ollama')
+    print(f"Using Ollama at {OLLAMA_BASE_URL} with model: {MODEL}")
 
 def get_links_user_prompt(url):
     user_prompt = f"""
