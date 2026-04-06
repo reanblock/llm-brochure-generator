ffe94ebc15385aed4999ae1756ed1a5a3044f279
darren <darren.jensen@gmail.com>
2026-04-06 19:20:14 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Website to Brochure Generator
+
+Convert any website into a marketing brochure.
+
+## Install and Run
+
+```bash
+uv run main.py
+```
+
+You will be asked to "Enter a URL to use as content generate the brochure:". As an example try `https://reanblock.com`.
---
+++ main.py
@@ -0,0 +1,88 @@
+"""
+Website to Brochure Generator
+"""
+
+import os
+import json
+from dotenv import load_dotenv
+from scraper import fetch_website_links, fetch_website_contents
+from prompts import link_system_prompt, brochure_system_prompt
+from openai import OpenAI
+
+load_dotenv()
+
+OLLAMA_BASE_URL = os.getenv("OLLAMA_BASE_URL", "http://localhost:11434/v1")
+MODEL = os.getenv("MODEL", "llama3.1:8b")
+
+openai = OpenAI(base_url=OLLAMA_BASE_URL, api_key='ollama')
+print(f"Using model: {MODEL}")
+print(f"Using Ollama URL:: {OLLAMA_BASE_URL}")
+
+def get_links_user_prompt(url):
+    user_prompt = f"""
+Here is the list of links on the website {url} -
+Please decide which of these are relevant web links for a brochure about the company, 
+respond with the full https URL in JSON format.
+Do not include Terms of Service, Privacy, email links.
+
+Links (some might be relative links):
+
+"""
+    links = fetch_website_links(url)
+    user_prompt += "\n".join(links)
+    return user_prompt
+
+def select_relevant_links(url):
+    # print(f"Selecting relevant links for {url} by calling {MODEL}")
+    response = openai.chat.completions.create(
+        model=MODEL,
+        messages=[
+            {"role": "system", "content": link_system_prompt},
+            {"role": "user", "content": get_links_user_prompt(url)}
+        ],
+        response_format={"type": "json_object"}
+    )
+    result = response.choices[0].message.content
+    links = json.loads(result)
+    # print(f"Found {len(links['links'])} relevant links")
+    return links
+
+def fetch_page_and_all_relevant_links(url):
+    contents = fetch_website_contents(url)
+    relevant_links = select_relevant_links(url)
+    result = f"## Landing Page:\n\n{contents}\n## Relevant Links:\n"
+    for link in relevant_links['links']:
+        result += f"\n\n### Link: {link['type']}\n"
+        result += fetch_website_contents(link["url"])
+    return result
+
+def get_brochure_user_prompt(company_name, url):
+    user_prompt = f"""
+You are looking at a company called: {company_name}
+Here are the contents of its landing page and other relevant pages;
+use this information to build a short brochure of the company in markdown without code blocks.\n\n
+"""
+    user_prompt += fetch_page_and_all_relevant_links(url)
+    user_prompt = user_prompt[:5_000] # Truncate if more than 5,000 characters
+    return user_prompt
+
+def create_brochure(company_name, url):
+    response = openai.chat.completions.create(
+        model=MODEL,
+        messages=[
+            {"role": "system", "content": brochure_system_prompt},
+            {"role": "user", "content": get_brochure_user_prompt(company_name, url)}
+        ],
+    )
+    result = response.choices[0].message.content
+    print(result)
+
+def main():
+    """Main entry point for testing."""
+    url = input("Enter a URL to use as content generate the brochure: ")
+    print("\nFetching and generating...\n")
+    create_brochure("Example Company", url)
+
+
+if __name__ == "__main__":
+    main()
